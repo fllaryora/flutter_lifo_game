3d63079c0fb91afb29912ce390dbd541a43fc403
Francisco <fllaryora@gmail.com>
2022-12-11 19:38:29 -0300

    la animacion es una mierda

--- lifo_app/lib/view/solver.screen.dart
+++ lifo_app/lib/view/solver.screen.dart
@@ -39,100 +39,116 @@ class _SolverPageState extends State<SolverPage> {
     return Scaffold(
       backgroundColor: const Color(0xfffffaeb),
       appBar: AppBar(title: Text(widget.title),),
-      body: Column(
-            //start == top
-            mainAxisAlignment: MainAxisAlignment.start,
-            //start == leading / left
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: ratio/4.0,
-                    left: ratio/2.0, right: ratio/2.0),
-                child: const Text(
-                  'Solution',
+      body: SingleChildScrollView(
+        child: Column(
+              //start == top
+              mainAxisAlignment: MainAxisAlignment.start,
+              //start == leading / left
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: ratio/4.0,
+                      left: ratio/2.0, right: ratio/2.0),
+                  child: const Text(
+                    'Solution',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.normal,
+                          fontSize: 55,
+                          fontFamily: 'Misteryzero',
+                        color: Color(0xff9d916b)
+                      )
+                  ),
+                ),
+                const Text(
+                    'Move',
                     textAlign: TextAlign.center,
                     style: TextStyle(fontWeight: FontWeight.normal,
-                        fontSize: 55,
-                        fontFamily: 'Misteryzero',
-                      color: Color(0xff9d916b)
+                        fontSize: 25,
+                        color: Color(0xff9d916b)
                     )
                 ),
-              ),
-              Container(
-                color: Colors.deepOrangeAccent,
-                child: SizedBox(
-                  width: width,
-                  height: stackHeight,
-                  child: Stack(
-                    children: drawScenario(stackHeight, stackWidth,
-                        ratio),
+                SizedBox(
+                  height: ratio/4,
+                ),
+                Container(
+                  color: Colors.deepOrangeAccent,
+                  child: SizedBox(
+                    width: width,
+                    height: stackHeight,
+                    child: Stack(
+                      children: drawScenario(stackHeight, stackWidth,
+                          ratio),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: ratio/4,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Material(
-                    color: Colors.blueGrey.shade300,
-                    borderRadius: BorderRadius.circular(ratio*2.5),
-                    child: InkWell(
-                      onTap: () {
-                        if(currentScenario > 0){
-                          setState(() {
-                            currentScenario = currentScenario - 1;
-                          });
-                        }
-                      },
+                SizedBox(
+                  height: ratio/4,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Material(
+                      color: Colors.blueGrey.shade300,
                       borderRadius: BorderRadius.circular(ratio*2.5),
-                      child: Container(
-                        width: ratio * 4.0,
-                        height: ratio,
-                        alignment: Alignment.center,
-                        child: const Text('Prev',
-                            style: TextStyle(fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.white
-                            )
+                      child: InkWell(
+                        onTap: () {
+                          if(currentScenario > 0){
+                            setState(() {
+                              currentScenario = currentScenario - 1;
+                            });
+                          }
+                        },
+                        borderRadius: BorderRadius.circular(ratio*2.5),
+                        child: Container(
+                          width: ratio * 4.0,
+                          height: ratio,
+                          alignment: Alignment.center,
+                          child: const Text('Prev',
+                              style: TextStyle(fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.white
+                              )
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: ratio/2,
-                  ),
-                  Material(
-                    color: Colors.blueGrey.shade300,
-                    borderRadius: BorderRadius.circular(ratio*2.5),
-                    child: InkWell(
-                      onTap: () {
-                        if(currentScenario < (widget.solution.length - 1)){
-                          setState(() {
-                            currentScenario = currentScenario + 1;
-                          });
-                        }
-                      },
+                    SizedBox(
+                      width: ratio/2,
+                    ),
+                    Material(
+                      color: Colors.blueGrey.shade300,
                       borderRadius: BorderRadius.circular(ratio*2.5),
-                      child: Container(
-                        width: ratio * 4.0,
-                        height: ratio,
-                        alignment: Alignment.center,
-                        child: const Text('Next',
-                            style: TextStyle(fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.white
-                            )
+                      child: InkWell(
+                        onTap: () {
+                          if(currentScenario < (widget.solution.length - 1)){
+                            setState(() {
+                              currentScenario = currentScenario + 1;
+                            });
+                          }
+                        },
+                        borderRadius: BorderRadius.circular(ratio*2.5),
+                        child: Container(
+                          width: ratio * 4.0,
+                          height: ratio,
+                          alignment: Alignment.center,
+                          child: const Text('Next',
+                              style: TextStyle(fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.white
+                              )
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
+                  ],
+                ),
+                SizedBox(
+                  height: ratio/4,
+                ),
+              ],
+            ),
+      ),
     );
   }
 
